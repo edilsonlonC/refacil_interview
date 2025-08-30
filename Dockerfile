39b3cc58d839219fe901d1398d1cab56ade57b31
edilsonlonc <eddylson.londonio@gmail.com>
2025-08-30 14:16:01 -0500

    feat: kubectl deployment and service

--- Dockerfile
+++ Dockerfile
@@ -4,4 +4,4 @@ COPY package.json .
 COPY . .
 RUN npm install
 RUN npm run build
-CMD ["npm", "start"]
+CMD npm run migrate && npm run start
--- src/infrastructure/api/controllers/health/health.controller.ts
+++ src/infrastructure/api/controllers/health/health.controller.ts
@@ -1,10 +1,13 @@
 import { Request, Response } from 'express';
 import StatusCodes from 'http-status-codes';
 import { HealthModel } from '../../../../domain/models/health.model';
+import { createLogger } from '../../../logger';
 
 export class HealthController {
-  getHealth(_: Request, response: Response) {
+  private readonly logger = createLogger();
+  getHealth = (_: Request, response: Response) => {
     const healthModel = new HealthModel('OK', true);
+    this.logger.info(process.env.HOSTNAME);
     return response.status(StatusCodes.OK).json(healthModel);
-  }
+  };
 }
